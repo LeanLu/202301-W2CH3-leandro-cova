29e3e63c749e7c216169140d526b23da6a7fafb4
Leon <leandrolupano@hotmail.com>
2023-01-25 20:35:46 +0100

    Add shift-function

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { length, push, pop } from './functions.js';
+import { length, push, pop, shift } from './functions.js';
 
 describe('Given length function', () => {
   describe('When we have an Array', () => {
@@ -23,12 +23,23 @@ describe('Given push function', () => {
   });
 });
 
-describe.only('Given pop function', () => {
+describe('Given pop function', () => {
   describe('When we have an Array with n-elements', () => {
     test('Then the last element will be deleted will be returned', () => {
       const arrayTest = [1, 'pepe', true, 0, 'Cova'];
       const expected = 'Cova';
       const result = pop(arrayTest);
+      expect(result).toBe(expected);
+    });
+  });
+});
+
+describe.only('Given shift function', () => {
+  describe('When we have an Array with n-elements', () => {
+    test('Then the first element will be deleted will be returned', () => {
+      const arrayTest = [1, 'pepe', true, 0, 'Cova'];
+      const expected = 1;
+      const result = shift(arrayTest);
       expect(result).toBe(expected);
     });
   });
