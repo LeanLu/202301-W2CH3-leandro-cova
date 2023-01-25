4e9561618568516a5636feb904a04836a163fa72
Leon <leandrolupano@hotmail.com>
2023-01-25 19:38:54 +0100

    Add push-function

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,5 @@
 import { length } from './functions.js';
+import { push } from './functions.js';
 
 describe('Given length function', () => {
   describe('When we have an Array', () => {
@@ -6,6 +7,18 @@ describe('Given length function', () => {
       const arrayTest = [1, 'pepe', true, 0, 'Cova'];
       const expected = 5;
       const result = length(arrayTest);
+      expect(result).toBe(expected);
+    });
+  });
+});
+
+describe('Given push function', () => {
+  describe('When we have an Array and a new element', () => {
+    test('Then the element adds to the array and the length grows n-element', () => {
+      const arrayTest = [1, 'pepe', true, 0, 'Cova'];
+      const element = 'Leandro';
+      const expected = 6;
+      const result = push(arrayTest, element);
       expect(result).toBe(expected);
     });
   });
